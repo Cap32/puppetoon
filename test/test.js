b2947b1aba30557f927fcfa9d9b8800bad66fde5
3went2 <webb.png@gmail.com>
2017-10-11 16:43:31 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -1,23 +1,29 @@
 
 import Puppetoon from '../src/Puppetoon';
+// const Puppetoon = require('puppetoon');
 
 (async function main() {
-	const browser = await Puppetoon.connect({
-		url: 'ws://127.0.0.1:8808',
-	});
+	try {
+		const browser = await Puppetoon.connect({
+			url: 'ws://127.0.0.1:8808',
+		});
 
-	const { version } = await browser.version();
-	console.log('version', version);
+		const { version } = await browser.version();
+		console.log('version', version);
 
-	const page = await browser.newPage();
-	console.log('page created');
+		const page = await browser.newPage();
+		console.log('page created');
 
-	const { size } = await browser.getQueueSize();
-	console.log('queue size', size);
+		const { size } = await browser.getQueueSize();
+		console.log('queue size', size);
 
-	await page.close();
-	console.log('page closed');
+		await page.close();
+		console.log('page closed');
 
-	await browser.close();
-	console.log('browser closed');
+		await browser.close();
+		console.log('browser closed');
+	}
+	catch (err) {
+		console.error(err);
+	}
 }());
