1140b84b96cf8fcaaf9e91f68eb8621b38a53a04
3went2 <webb.png@gmail.com>
2017-10-12 14:46:24 +0800

    add Browser#runInPage()

--- src/Browser.js
+++ src/Browser.js
@@ -34,6 +34,28 @@ export default class Browser {
 		this._chrome && this._chrome.close();
 	}
 
+	async runInPage(fn, options = {}) {
+		const { maxTry = 3, ...pageOptions } = options;
+		if (maxTry < 1) { throw new Error('maxTry must large than 0'); }
+		const errors = [];
+		for (let index = 0; index < maxTry; index++) {
+			let page;
+			try {
+				page = await this.newPage(pageOptions);
+				const res = await fn(page, index);
+				await page.close();
+				return res;
+			}
+			catch (err) {
+				errors.push(err);
+				if (page) { await page.close().catch(() => {}); }
+			}
+		}
+		const error = errors[errors.length - 1];
+		error.errors = errors;
+		throw error;
+	}
+
 	async version() {
 		return this._api.version();
 	}
--- test/test.js
+++ test/test.js
@@ -3,8 +3,9 @@ import Puppetoon from '../src/Puppetoon';
 // const Puppetoon = require('puppetoon');
 
 (async function main() {
+	let browser;
 	try {
-		const browser = await Puppetoon.connect({
+		browser = await Puppetoon.connect({
 			url: 'ws://127.0.0.1:8808',
 		});
 
@@ -20,10 +21,17 @@ import Puppetoon from '../src/Puppetoon';
 		await page.close();
 		console.log('page closed');
 
+		await browser.runInPage(async (page, index) => {
+			if (index < 2) { throw new Error('you suck'); }
+			// await page.close();
+			return 'ok';
+		});
+
 		await browser.close();
 		console.log('browser closed');
 	}
 	catch (err) {
+		await browser.close().catch(() => {});
 		console.error(err);
 	}
 }());
