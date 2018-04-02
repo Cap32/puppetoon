8a82c365e6045d3323948236c9a108c2862a842d
3went2 <webb.png@gmail.com>
2018-04-02 18:30:58 +0800

    enhance Page object

--- src/Page.js
+++ src/Page.js
@@ -1,28 +1,32 @@
-
 import PuppeteerPage from 'puppeteer/lib/Page';
 
 export default class Page {
 	static async create(browser, targetId, closePage) {
-
 		// NOTE: uesing some internal properties from puppeteer Browser
 		const client = await browser._connection.createSession(targetId);
 		const page = await PuppeteerPage.create(
 			client,
 			browser._ignoreHTTPSErrors,
 			browser._appMode,
-			browser._screenshotTaskQueue,
+			browser._screenshotTaskQueue
 		);
 
-		return new Proxy({}, {
-			get(target, method) {
-				if (method === 'close') {
-					return async function close() {
-						if (!browser._connection) { return; }
-						return closePage();
-					};
+		return new Proxy(
+			{},
+			{
+				...Reflect,
+				get(target, method) {
+					if (method === 'close') {
+						return async function close() {
+							if (!browser._connection) {
+								return;
+							}
+							return closePage();
+						};
+					}
+					return page[method];
 				}
-				return page[method];
 			}
-		});
+		);
 	}
 }
